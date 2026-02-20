23faac4d06f83fdf7708b5ccc625dd923bd347e3
Pitt Wu <kgb00128@gmail.com>
2026-02-20 21:30:57 +0800

    [Chore][init] remove not used file

--- README.md
+++
@@ -1,1 +0,0 @@
-# Clone Reddit Example
--- src/components/Modal/Auth/SignUp.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-type SignUpProps = {};
-
-const SignUp: React.FC<SignUpProps> = () => {
-  return <div>Have a good coding</div>;
-};
-export default SignUp;
